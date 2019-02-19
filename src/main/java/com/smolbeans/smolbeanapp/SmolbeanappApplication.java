aa5e6e78b739ac5784b7fdfc4b4c0809c260ed47
Schijndel,Thomas T.G.H. van <t.vanschijndel@student.fontys.nl>
2019-02-19 10:47:14 +0100

    Initial commit

---
+++ src/main/java/com/smolbeans/smolbeanapp/SmolbeanappApplication.java
@@ -0,0 +1,13 @@
+package com.smolbeans.smolbeanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmolbeanappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmolbeanappApplication.class, args);
+    }
+
+}
