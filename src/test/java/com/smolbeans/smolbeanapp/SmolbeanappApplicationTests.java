87f0b22db30af2921dce40c83582ca7d23fb95f0
Hansen,Kristian K.T <kristian.hansen@student.fontys.nl>
2019-03-19 10:28:14 +0100

    travis testing 3

--- src/test/java/com/smolbeans/smolbeanapp/SmolbeanappApplicationTests.java
+++ src/test/java/com/smolbeans/smolbeanapp/SmolbeanappApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,10 @@ public class SmolbeanappApplicationTests {
 
     @Test
     public void contextLoads() {
+        String hello = null;
+        assertThat(hello).isNull();
     }
+
+
 
 }
